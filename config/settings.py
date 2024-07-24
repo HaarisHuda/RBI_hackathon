3683119934fa41e8f6dfe49fabad8df740dad76e
haaris <haarishuda090@gmail.com>
2024-07-24 23:45:55 +0530

    added allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-t0&bsur2_#32fqehv+0$u-o*z=
 
 DEBUG = True
 APPEND_SLASH = False
-ALLOWED_HOSTS = ['127.0.0.1', 'web-production-99b4c.up.railway.app/git']
+ALLOWED_HOSTS = ['127.0.0.1', 'web-production-99b4c.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://rbihackathon2024-production.up.railway.app']
 
 INSTALLED_APPS = [
